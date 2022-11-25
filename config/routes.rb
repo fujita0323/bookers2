2b70b7ded675a702ca2176327b26eb77b1894f14
EC2 Default User <ec2-user@ip-172-31-15-173.ap-northeast-1.compute.internal>
2022-11-25 15:47:45 +0000

    [bookers2]16章まで

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  devise_for :users
+  get 'homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+end
